1a6b5bd98a7d67fd07815d0df5134e9102ad2acd
Fitra Hanifah <116331462+Ifahanf@users.noreply.github.com>
2023-02-04 22:26:16 +0800

    Add files via upload

---
+++ Arduino_SKEL/src/main.cpp
@@ -0,0 +1,111 @@
+#include <DHT.h>
+#include <WiFi.h>
+
+#define DHT_SENSOR_PIN  2 // ESP32 pin GIOP21 connected to DHT11 sensor
+#define DHT_SENSOR_TYPE DHT11
+
+DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
+
+const char* ssid     = "@wifi.id";
+const char* password = "password0";
+
+const char* host = "34.143.181.48";
+
+
+void setup() {
+  Serial.begin(9600);
+  dht_sensor.begin(); // initialize the DHT sensor
+
+  delay(10);
+
+  // We start by connecting to a WiFi network
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // read humidity
+  float humi  = dht_sensor.readHumidity();
+  // read temperature in Celsius
+  float tempC = dht_sensor.readTemperature();
+  // read temperature in Fahrenheit
+  float tempF = dht_sensor.readTemperature(true);
+
+  // check whether the reading is successful or not
+  if ( isnan(tempC) || isnan(tempF) || isnan(humi)) {
+    Serial.println("Failed to read from DHT sensor!");
+  } else {
+    Serial.print("Humidity: ");
+    Serial.print(humi);
+    Serial.print("%");
+
+    Serial.print("  |  ");
+
+    Serial.print("Temperature: ");
+    Serial.print(tempC);
+    Serial.print("°C  ~  ");
+    Serial.print(tempF);
+    Serial.println("°F");
+  }
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 5000;
+  if (!client.connect(host, httpPort)) {
+      Serial.println("connection failed");
+      return;
+  }
+
+  // We now create a URI for the request
+  String url = "/insert";
+  url += "?temperature=";
+  url += String(tempC);
+  url += "&humidity=";
+  url += String(humi);
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+      if (millis() - timeout > 5000) {
+          Serial.println(">>> Client Timeout !");
+          client.stop();
+          return;
+      }
+  }
+
+  // Read all the lines of the reply from server and print them to Serial
+  while(client.available()) {
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+  }
+
+  Serial.println();
+  Serial.println("closing connection");
+
+  // wait a 15 seconds between readings
+  delay(15000);
+}
